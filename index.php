70065866d058a4072fde1fe45969007071b914ff
sharathkv <sharathkv@dckap>
2022-05-05 15:55:05 +0530

    mail comment

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $to = "sharathkv@dckap.com";
 $subject = "My subject";
 $txt = "Hello world!";
 $headers = "From: sharathkumarv183@gmail.com"; 
-mail('sharathkv@dckap.com','My subject','Hello world!',$headers);
+mail($to,$subject,$txt,$headers);
 
 $errorMessage = error_get_last()['message'];
 ?>
