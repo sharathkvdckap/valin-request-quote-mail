4d02d7a807bc5456cbad18122605bb480b86c120
sharathkv <sharathkv@dckap>
2022-05-05 15:50:33 +0530

    mail comment

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 
 
 <?php
-echo "start";
+
 $to = "sharathkv@dckap.com";
 $subject = "My subject";
 $txt = "Hello world!";
 $headers = "From: sharathkumarv183@gmail.com"; 
 mail($to,$subject,$txt,$headers);
-echo "end";
+
+$errorMessage = error_get_last()['message'];
 ?>
