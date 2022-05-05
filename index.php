d5d9b722fa52002448bf1164d1a359a6a6c60f7b
sharathkv <sharathkv@dckap>
2022-05-05 15:44:13 +0530

    mail comment

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
+
+
 <?php
-// if(isset($_POST['message']))
- 
-    $to      = 'sharathkv@dckap.com';
-    $subject = 'Request for Quote';  
-    $sendername = 'sharath@gmail.com';  
-    $name = 'name'; 
-    $company = 'company'; 
-    $email = 'email'; 
-    $phone_no = 'phone_no'; 
-    $partNum = 'partNum'; 
-    $PartQty = 'PartQty';
-    $message = "'Name:'.$name. '<br>' 'Company:'.$company. '<br>' 'Email:'.$email. '<br>' 'Phone:'.$phone_no.";
-    $headers = "From: ".$sendername." \r\n";
-    // $headers = "Reply-To: ".$_POST['sender_ema‌​il‌​']."\r\n"; 
-    $headers = "Content-type: text/html; charset=iso-8859-1\r\n";
-    'X-Mailer: PHP/' . phpversion();
-    mail($to, $subject, $message, $headers);
-  
+$to = "sharathkv@dckap.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: sharath@gamil.com"; 
+mail($to,$subject,$txt,$headers);
 ?>
