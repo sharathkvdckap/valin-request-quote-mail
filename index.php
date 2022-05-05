3dbf25160c983005417cb001f8911e68f4e90613
sharathkv <sharathkv@dckap>
2022-05-05 15:36:37 +0530

    mail comment

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ if($_POST['submit_request_quote'] == 'submit')
   {
     $to      = 'sharathkv@dckap.com';
     $subject = 'Request for Quote';  
-    $sendername = 'sharath';  
-    $name = $_POST['name']; 
-    $company = $_POST['company']; 
-    $email = $_POST['email']; 
-    $phone_no = $_POST['phone_no']; 
-    $partNum = $_POST['partNum']; 
-    $PartQty = $_POST['PartQty'];
+    $sendername = 'sharath@gmail.com';  
+    $name = 'name'; 
+    $company = 'company'; 
+    $email = 'email'; 
+    $phone_no = 'phone_no'; 
+    $partNum = 'partNum'; 
+    $PartQty = 'PartQty';
     $message = "'Name:'.$name. '<br>' 'Company:'.$company. '<br>' 'Email:'.$email. '<br>' 'Phone:'.$phone_no.";
     $headers = "From: ".$sendername." \r\n";
     // $headers = "Reply-To: ".$_POST['sender_ema‌​il‌​']."\r\n"; 
