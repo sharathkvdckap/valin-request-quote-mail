5a6437e41076420a1da3bfef94f7ec20fc50d38f
sharathkv <sharathkv@dckap>
2022-05-05 15:45:14 +0530

    mail comment

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 
 
 <?php
+echo "start";
 $to = "sharathkv@dckap.com";
 $subject = "My subject";
 $txt = "Hello world!";
 $headers = "From: sharath@gamil.com"; 
 mail($to,$subject,$txt,$headers);
+echo "end";
 ?>
